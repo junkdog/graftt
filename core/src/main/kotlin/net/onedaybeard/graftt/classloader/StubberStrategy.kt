eddd19f129d7f914dc7fe0ba2077a5aefadbc6dc
Adrian Papari <a@onedaybeard.net>
2019-06-15 00:41:08 +0200

    remove AsmClassLoader

--- core/src/main/kotlin/net/onedaybeard/graftt/classloader/StubberStrategy.kt
+++
@@ -1,9 +0,0 @@
-package net.onedaybeard.graftt.classloader
-
-import net.onedaybeard.graftt.toBytes
-import org.objectweb.asm.tree.ClassNode
-
-interface AsmStrategy {
-    fun stubClass(name: String): ByteArray
-}
-
