8653445ecefed46bc796f06e87eae23673a3c5c5
Adrian Papari <a@onedaybeard.net>
2019-06-20 22:17:21 +0200

    inline classfile transformer and remove explicit class from agent

--- agent/src/main/kotlin/net/onedaybeard/graftt/agent/GraftAgent.kt
+++ agent/src/main/kotlin/net/onedaybeard/graftt/agent/GraftAgent.kt
@@ -1,11 +1,45 @@
 package net.onedaybeard.graftt.agent
 
+import com.github.michaelbull.result.map
+import com.github.michaelbull.result.onFailure
+import com.github.michaelbull.result.onSuccess
+import com.github.michaelbull.result.unwrap
+import net.onedaybeard.graftt.`(╯°□°）╯︵ ┻━┻`
+import net.onedaybeard.graftt.classNode
+import net.onedaybeard.graftt.graft.performGraft
+import net.onedaybeard.graftt.graft.readRecipientType
+import net.onedaybeard.graftt.toBytes
+import org.objectweb.asm.tree.ClassNode
+import java.lang.instrument.ClassFileTransformer
 import java.lang.instrument.Instrumentation
+import java.security.ProtectionDomain
 
-class GraftAgent  {
-    companion object {
-        @JvmStatic fun premain(agentArgs: String?, inst: Instrumentation) {
-            inst.addTransformer(TransplantTransformer())
+fun premain(agentArgs: String?, inst: Instrumentation) {
+    inst.addTransformer(object : ClassFileTransformer {
+        val transplants: MutableMap<String, ClassNode> = mutableMapOf()
+
+        init {
+            println("graftt agent preparing for surgery...")
         }
-    }
+
+        override fun transform(
+            loader: ClassLoader?,
+            className: String,
+            classBeingRedefined: Class<*>?,
+            protectionDomain: ProtectionDomain?,
+            classfileBuffer: ByteArray
+        ): ByteArray {
+            val cn = classNode(classfileBuffer)
+
+            readRecipientType(cn)
+                .onSuccess { transplants[it.internalName] = cn }
+
+            return transplants[className]?.let { donor ->
+                performGraft(donor, classNode(classfileBuffer))
+                    .map(ClassNode::toBytes)
+                    .onFailure(`(╯°□°）╯︵ ┻━┻`)
+                    .unwrap()
+            } ?: classfileBuffer
+        }
+    })
 }
