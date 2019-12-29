8545e41fe2599f6e43a70176963a3759cd6ff7aa
Adrian Papari <a@onedaybeard.net>
2019-12-29 17:01:40 +0100

    add missing test class

---
+++ core/src/test/java/net/onedaybeard/graftt/SubstituteAnnoValues.java
@@ -0,0 +1,15 @@
+package net.onedaybeard.graftt;
+
+
+public interface SubstituteAnnoValues {
+    @interface AA { Class<?>[] value(); }
+
+    @AA(BarTransplant.class) // expect AA(Bar.class) after transplant
+    @Graft.Recipient(Foo.class)
+    class FooTransplant {}
+    class Foo {}
+
+    @Graft.Recipient(Bar.class)
+    class BarTransplant {}
+    class Bar {}
+}
